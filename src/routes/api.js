392558d21b67843e77f48e65e5e3dfddef713e61
Mir Raisul Tanvir <tanvirgraphic@gmail.com>
2023-12-10 03:36:36 +0600

    Updated

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const ProductController = require("../controllers/ProductController");
-
-
-// //Product API
-// router.post("/addProduct", ProductController.AddProduct)
-// router.post("/adduser", ProductController.AddProduct)
-
-
-// // Order API
-// router.post("/order", ProductController.AddOrder)
 
 
 
